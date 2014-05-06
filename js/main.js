b791e9d329ae3f4e55db2ce091bbd271a57a3258
lintonhale <lintonhale@gmail.com>
2014-05-06 16:30:30 -0700

    First installment...

---
+++ js/main.js
@@ -0,0 +1,57 @@
+var app = {
+
+	renderHomeView: function() {
+	    var html =
+	            "<div class='header'><h1>Homerun</h1></div>" +
+	            "<div class='search-view'>" +
+	            "<input class='search-key'/>" +
+	            "<ul class='employee-list'></ul>" +
+	            "</div>"
+	    $('body').html(html);
+	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
+
+	showAlert: function (message, title) {
+	    if (navigator.notification) {
+	        navigator.notification.alert(message, null, title, 'OK');
+	    } else {
+	        alert(title ? (title + ": " + message) : message);
+	    }
+	},
+	
+    findByName: function() {
+        console.log('findByName');
+        this.store.findByName($('.search-key').val(), function(employees) {
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i=0; i<l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
+        });
+    },
+
+	initialize: function() {
+	    var self = this;
+	    this.store = new MemoryStore(function() {
+	        self.renderHomeView();
+	    });
+	}
+	/////	SECOND VERSION
+	//	initialize: function() {
+	//	    var self = this;
+	//	    this.store = new MemoryStore(function() {
+	//	        self.showAlert('This is an important message.', 'Welcome!');
+	//	    });
+	//	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	//	}
+	///// ORIGINAL VERSION:
+	//    initialize: function() {
+	//        this.store = new MemoryStore();
+	//        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	//    }
+
+};
+
+app.initialize();
