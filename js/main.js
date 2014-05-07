c1be10a4ac7d7e8147f2559b63b585b313a6c14c
lintonhale <lintonhale@gmail.com>
2014-05-06 21:42:36 -0700

    master after pt 6

--- js/main.js
+++ js/main.js
@@ -1,33 +1,19 @@
 var app = {
 
-	renderHomeView: function() {
-	    $('body').html(this.homeTpl());
-	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
-	showAlert: function (message, title) {
-	    if (navigator.notification) {
-	        navigator.notification.alert(message, null, title, 'OK');
-	    } else {
-	        alert(title ? (title + ": " + message) : message);
-	    }
-	},
-	
-	findByName: function() {
-	    var self = this;
-	    this.store.findByName($('.search-key').val(), function(employees) {
-	        $('.employee-list').html(self.employeeLiTpl(employees));
-	    });
-	},
-
-	initialize: function() {
-	    var self = this;
-	    this.store = new MemoryStore(function() {
-	        self.renderHomeView();
-	    });
-	    this.homeTpl = Handlebars.compile($("#home-tpl").html());
-	    this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-	}
+    initialize: function() {
+        var self = this;
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
+        });
+    }
 
 };
 
