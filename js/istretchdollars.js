aba8a7f0d802b186702e436f4946538293fa30f3
lintonhale <lintonhale@gmail.com>
2014-05-15 19:49:16 -0700

    iuh

--- js/istretchdollars.js
+++ js/istretchdollars.js
@@ -82,7 +82,7 @@
                 var element3 = document.createElement("select");
 				element3.setAttribute("name", "mySelect");
 				element3.setAttribute("id", "mySelect");
-				element3.style.width = "15px";
+				element3.style.width = "105px";
 				// setting an onchange event
 				element3.onchange = function() {saveItemList()};			 
 				var option;
